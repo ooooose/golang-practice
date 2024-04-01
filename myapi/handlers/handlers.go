945414a531283475fd756e7352da7ce37bbe96af
ooooose <yuuki.oose1990@gmail.com>
2024-04-02 08:43:56 +0900

    リファクタ

--- myapi/handlers/handlers.go
+++ myapi/handlers/handlers.go
@@ -18,25 +18,12 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
-	if err != nil {
-		http.Error(w, "cannot get content length\n", http.StatusBadRequest)
-		return
-	}
-	reqBodybuffer := make([]byte, length)
-
-	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
-		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
-		return
-	}
-	defer req.Body.Close()
 
 	var reqArticle models.Article
-	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
-		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
-		return
-	}
 
+  if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+    http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+  }
 	//fmt.Printf("%+v\n", reqArticle)
 
 	article := reqArticle
