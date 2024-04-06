de32712100d9539baa39ac6bb62cd37021404d7e
ooooose <yuuki.oose1990@gmail.com>
2024-04-06 12:43:00 +0900

    Add insert処理の実装

--- exec-db/main.go
+++ exec-db/main.go
@@ -20,31 +20,24 @@ func main() {
     }
     defer db.Close()
 
-    articleID := 1
-    const sqlStr = `
-        select *
-        from articles
-        where article_id = ?;
-    `
-
-    row := db.QueryRow(sqlStr, articleID)
-    if err := row.Err(); err != nil {
-        fmt.Println(err)
-        return
+    article := models.Article{
+        Title: "insert test",
+        Contents: "Can I insert data correctly?",
+        UserName: "oose",
     }
 
-    var article models.Article
-    var createdTime sql.NullTime
+    const sqlStr = `
+        insert into articles (title, contents, username, nice, created_at) values
+        (?, ?, ?, 0, now());
+    `
 
-    err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+    result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+
     if err != nil {
         fmt.Println(err)
         return
     }
 
-    if createdTime.Valid {
-        article.CreatedAt = createdTime.Time
-    }
-
-    fmt.Printf("%+v\n", article)
+    fmt.Println(result.LastInsertId())
+    fmt.Println(result.RowsAffected())
 }
