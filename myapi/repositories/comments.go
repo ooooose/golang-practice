7a643bd3cc0f4d599388b9c4855387b4b290f1a0
ooooose <yuuki.oose1990@gmail.com>
2024-04-07 00:46:13 +0900

    テスト記述

--- myapi/repositories/comments.go
+++ myapi/repositories/comments.go
@@ -1,43 +1,57 @@
 package repositories
 
 import (
-    "database/sql"
+	"database/sql"
 
-    "github.com/yourname/reponame/models"
+	"github.com/yourname/reponame/models"
 )
 
+// 新規投稿をDBにinsertする関数
 func InsertComment(db *sql.DB, comment models.Comment) (models.Comment, error) {
-    const sqlStr = `
-        insert into comments (article_id, message, created_at) values
-        (?, ?, now());
-    `
+	const sqlStr = `
+		insert into comments (article_id, message, created_at) values
+		(?, ?, now());
+	`
+	var newComment models.Comment
+	newComment.ArticleID, newComment.Message = comment.ArticleID, comment.Message
 
-    var newComment models.Comment
-    newComment.ArticleID, newComment.Message = comment.ArticleID, comment.Message
+	result, err := db.Exec(sqlStr, comment.ArticleID, comment.Message)
+	if err != nil {
+		return models.Comment{}, err
+	}
 
-    result, err := db.Exec(sqlStr, comment.ArticleID, comment.Massage)
-    if err != nil {
-        return models.Comment{}, err
-    }
+	id, _ := result.LastInsertId()
+	newComment.CommentID = int(id)
 
-    if err != nil {
-        return nil, err
-    }
+	return newComment, nil
+}
 
-    defer rows.Close()
+// 指定IDの記事についたコメント一覧を取得する関数
+func SelectCommentList(db *sql.DB, articleID int) ([]models.Comment, error) {
+	const sqlStr = `
+		select *
+		from comments
+		where article_id = ?;
+	`
 
-    commentArray := make([]models.Comment, 0)
-    for rows.Next() {
-        var comment models.Comment
-        var createdTime sql.NullTime
-        rows.Scan(&comment.CommentID, &comment.ArticleID, &comment.Message, &createdTime)
+	rows, err := db.Query(sqlStr, articleID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-        if createdTime.Valid {
-            comment.CreatedAt = createdTime.TIme
-        }
+	commentArray := make([]models.Comment, 0)
+	for rows.Next() {
+		var comment models.Comment
+		var createdTime sql.NullTime
+		rows.Scan(&comment.CommentID, &comment.ArticleID, &comment.Message, &createdTime)
 
-        commentArray = append(commentArray, comment)
-    }
+		if createdTime.Valid {
+			comment.CreatedAt = createdTime.Time
+		}
 
-    return commentArray, nil
+		commentArray = append(commentArray, comment)
+	}
+
+	return commentArray, nil
 }
