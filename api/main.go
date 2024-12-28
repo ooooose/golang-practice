e5f7588a5488eac7261ee4dc1efea39cd650604b
ooooose <yuuki.oose1990@gmail.com>
2024-12-28 17:05:46 +0900

    Add エンドポイントを追加

--- api/main.go
+++ api/main.go
@@ -12,11 +12,31 @@ func main() {
 	}
 
 	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Post Article...\n")
+		io.WriteString(w, "Posting Article...\n")
+	}
+
+	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article List\n")
+	}
+
+	articleNo1Handler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article No.1\n")
+	}
+	
+	articleNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article Nice...\n")
+	}
+
+	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Comment...\n")
 	}
 
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/1", articleNo1Handler)
+	http.HandleFunc("/article/nice", articleNiceHandler)
+	http.HandleFunc("/comment", postCommentHandler)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
