097c1ff2c99881cf3698dfe91134bd69693b8c74
ooooose <yuuki.oose1990@gmail.com>
2025-01-02 17:13:24 +0900

    Add デコーダ・エンコーダを使ったリファクタ

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"strconv"
 
-	"errors"
 	"github.com/gorilla/mux"
 	"github.com/yourname/reponame/models"
 )
@@ -17,36 +16,16 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
-	if err != nil {
-		http.Error(w, "Invalid Content-Length", http.StatusBadRequest)
-		return
-	}
-
-	reqBodybuffer := make([]byte, length)
-
-	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
-		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
-		return
-	}
-
-	defer req.Body.Close()
-
 	var reqArticle models.Article
-	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
-		return
+
 	}
 
 	article := reqArticle
-	jsonData, err := json.Marshal(article)
 
-	if err != nil {
-		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(jsonData)
+	json.NewEncoder(w).Encode(article)
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
