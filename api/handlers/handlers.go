545083bd4e78853c1740d92cf64cd4ff6452f0ed
ooooose <yuuki.oose1990@gmail.com>
2025-01-02 17:36:05 +0900

    Fix 全てのAPIを修正・リファクタ

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -11,16 +11,16 @@ import (
 	"github.com/yourname/reponame/models"
 )
 
+// GET /hello のハンドラ
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello, world!\n")
 }
 
+// POST /article のハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle models.Article
-
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
-
 	}
 
 	article := reqArticle
@@ -28,27 +28,31 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(article)
 }
 
+// GET /article/list のハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	queryMap := req.URL.Query()
-	var page int
 
+	// クエリパラメータpageを取得
+	var page int
 	if p, ok := queryMap["page"]; ok && len(p) > 0 {
 		var err error
 		page, err = strconv.Atoi(p[0])
 		if err != nil {
 			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 			return
-		} 
+		}
 	} else {
 		page = 1
 	}
 
+	// 「変数pageが使われていない」というコンパイルエラーを回避
 	log.Println(page)
-	
+
 	articleList := []models.Article{models.Article1, models.Article2}
 	json.NewEncoder(w).Encode(articleList)
 }
 
+// GET /article/{id} のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
@@ -56,17 +60,32 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// 「変数articleIDが使われていない」というコンパイルエラーを回避
 	log.Println(articleID)
 
 	article := models.Article1
-	
+
 	json.NewEncoder(w).Encode(article)
 }
 
+// POST /article/nice のハンドラ
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article Nice...\n")
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+
+	article := reqArticle
+	json.NewEncoder(w).Encode(article)
 }
 
+// POST /comment のハンドラ
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Comment...\n")
+	var reqComment models.Comment
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+
+	comment := reqComment
+	json.NewEncoder(w).Encode(comment)
 }
