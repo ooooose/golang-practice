3e80ddea6d22e56bb3d28b7052c42268a9243cac
ooooose <yuuki.oose1990@gmail.com>
2025-01-03 17:11:05 +0900

    Add ドライバのインストール

--- api/main.go
+++ api/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
@@ -8,6 +12,24 @@ import (
 )
 
 func main() {
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	db, err :=sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
