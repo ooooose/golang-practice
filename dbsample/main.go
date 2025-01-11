9b5615ee5059d8d7aa5955aff01848afe7fdfa98
ooooose <yuuki.oose1990@gmail.com>
2025-01-12 06:32:52 +0900

    Add トランザクションを利用したDB操作を実装

--- dbsample/main.go
+++ dbsample/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"dbsample/models"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -50,6 +49,37 @@ import (
 // 	fmt.Printf("%+v\n", article)
 // }
 
+// func main() {
+// 	dbUser := "docker"
+// 	dbPassword := "docker"
+// 	dbDatabase := "sampledb"
+// 	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+// 	db, err := sql.Open("mysql", dbConn)
+	
+// 	if err != nil {
+// 	fmt.Println(err)
+// 	}
+// 	defer db.Close()
+
+// 	article := models.Article{
+// 	Title: "insert test",
+// 	Contents: "Can I insert data correctly?",
+// 	UserName: "oose",
+// 	}
+// 	const sqlStr =`
+// 		insert into articles (title, contents, username, nice, created_at) values
+// 		(?, ?, ?, 0, now());
+// 	`
+// 	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	fmt.Println(result.LastInsertId())
+// 	fmt.Println(result.RowsAffected())
+// }
+
 func main() {
 	dbUser := "docker"
 	dbPassword := "docker"
@@ -62,21 +92,43 @@ func main() {
 	}
 	defer db.Close()
 
-	article := models.Article{
-	Title: "insert test",
-	Contents: "Can I insert data correctly?",
-	UserName: "oose",
-	}
-	const sqlStr =`
-		insert into articles (title, contents, username, nice, created_at) values
-		(?, ?, ?, 0, now());
-	`
-	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	tx, err := db.Begin()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(result.LastInsertId())
-	fmt.Println(result.RowsAffected())
+	article_id := 1
+	const sqlGetNice = `
+		select nice
+		from articles
+		where article_id = ?;
+	`
+
+	row := tx.QueryRow(sqlGetNice, article_id)
+
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	var nicenum int
+	err = row.Scan(&nicenum)
+
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	const sqlUpdateNice = `update articles set nice = ? where article_id = ?`
+	_, err = tx.Exec(sqlUpdateNice, nicenum + 1, article_id)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	tx.Commit()
 }
