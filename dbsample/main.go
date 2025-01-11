322a3cf00ba0a7f87ae78b6e730362374a6f1d71
ooooose <yuuki.oose1990@gmail.com>
2025-01-12 06:20:25 +0900

    Fix Insert処理を実装

--- dbsample/main.go
+++ dbsample/main.go
@@ -8,44 +8,75 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// func main() {
+// 	dbUser := "docker"
+// 	dbPassword := "docker"
+// 	dbDatabase := "sampledb"
+// 	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+// 	db, err := sql.Open("mysql", dbConn)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	defer db.Close()
+
+// 	articleID := 1
+// 	const sqlStr = `
+// 		select *
+// 		from articles
+// 		where article_id = ?;
+// 	`
+
+// 	row := db.QueryRow(sqlStr, articleID)
+// 	if err := row.Err(); err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	var article models.Article
+// 	var createdTime sql.NullTime
+
+// 	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	if createdTime.Valid {
+// 		article.CreatedAt = createdTime.Time
+// 	}
+
+// 	fmt.Printf("%+v\n", article)
+// }
+
 func main() {
 	dbUser := "docker"
 	dbPassword := "docker"
 	dbDatabase := "sampledb"
 	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-
 	db, err := sql.Open("mysql", dbConn)
+	
 	if err != nil {
-		fmt.Println(err)
+	fmt.Println(err)
 	}
-
 	defer db.Close()
 
-	articleID := 1
-	const sqlStr = `
-		select *
-		from articles
-		where article_id = ?;
-	`
-
-	row := db.QueryRow(sqlStr, articleID)
-	if err := row.Err(); err != nil {
-		fmt.Println(err)
-		return
+	article := models.Article{
+	Title: "insert test",
+	Contents: "Can I insert data correctly?",
+	UserName: "oose",
 	}
-
-	var article models.Article
-	var createdTime sql.NullTime
-
-	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	const sqlStr =`
+		insert into articles (title, contents, username, nice, created_at) values
+		(?, ?, ?, 0, now());
+	`
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	if createdTime.Valid {
-		article.CreatedAt = createdTime.Time
-	}
-
-	fmt.Printf("%+v\n", article)
+	fmt.Println(result.LastInsertId())
+	fmt.Println(result.RowsAffected())
 }
